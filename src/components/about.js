c486db8cea4acd74ba8c9bf8aa969ad8172d1d03
Heather Stock <33641095+heatherstock@users.noreply.github.com>
2020-11-20 16:26:27 +0000

    Add about content

--- src/components/about.js
+++ src/components/about.js
@@ -34,20 +34,33 @@ const AboutPage = () => (
   <Wrapper>
     <Heading id='about'>About</Heading>
     <Content style={style}>
-      Drayson is a digital designer and letterpress printer with a passion for beautiful typography and a 
-        thing for nice paper. He creates beautiful letterpress wedding stationery, greetings cards and prints 
-        using antique type and printing equipment, and is usually found covered in ink or oil (or both!).
-      </Content>
-      <Content>
-        Drayson is a digital designer and letterpress printer with a passion for beautiful typography and a 
-        thing for nice paper. He creates beautiful letterpress wedding stationery, greetings cards and prints 
-        using antique type and printing equipment, and is usually found covered in ink or oil (or both!).
-      </Content> 
-      <Content>
-        Drayson is a digital designer and letterpress printer with a passion for beautiful typography and a 
-        thing for nice paper. He creates beautiful letterpress wedding stationery, greetings cards and prints 
-        using antique type and printing equipment, and is usually found covered in ink or oil (or both!).
-      </Content> 
+      I am passionate about design and typography and love crafting great digital experiences: 
+      from ideation to low fidelity wireframes to fully responsive user interactions, 
+      driven by real data and user insight. I am experienced in working with small design 
+      studios as well as working as part of cross functional, agile, product development 
+      teams within larger companies, forming tight knit relationships with other disciplines 
+      to fully realise ideas and create beautiful work.
+    </Content>
+    <Content>
+      I live and breathe typography; it’s the first thing someone notices and reads when 
+      discovering a brand or product, although it’s a fundamental often overlooked. 
+      I’m happiest when creating clear visual hierarchies and making the most of typographic 
+      subtleties to ensure a great user experience.
+    </Content>
+    <Content>
+      To maintain a balance between digital and analogue, I can often be found covered in ink 
+      and oil, setting wood and metal type and printing various letterpress things using antique 
+      equipment. This passion of mine lets me disconnect digitally (the cabinets are full of 
+      lead type which helps!) and get lost in the slow but rewarding process that letterpress 
+      printing fosters, such as handling the white space and fiddling with hair spacing to 
+      ensure the typographic details are just so. You can see some of my journey and the 
+      ever-growing collection of type and equipment on Instagram (@draysonandstock).
+    </Content>
+    <Content>
+      My latest obsession is gardening and I’m currently turning my front garden by the sea into 
+      a fully functioning kitchen garden complete with 17 fruit trees (so far), while 
+      encouraging as much wildlife as possible. 
+    </Content>
   </Wrapper>
 )
 
